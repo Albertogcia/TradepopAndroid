573e990a8c187e13d50c10ae424d1821e3dbbd12
Alberto García Antuña <garantalberto@gmail.com>
2021-10-07 23:02:47 +0200

    Kodein

---
+++ app/src/main/java/com/alberto/tradepop/di/App.kt
@@ -0,0 +1,14 @@
+package com.alberto.tradepop.di
+
+import android.app.Application
+import org.kodein.di.DI
+import org.kodein.di.DIAware
+
+class App: Application(), DIAware {
+
+    override val di: DI by DI.lazy {
+        import(AppDIModule(application = this@App).create())
+        import(ViewModelDIModule.create())
+    }
+
+}
---
+++ app/src/main/java/com/alberto/tradepop/di/AppDIModule.kt
@@ -0,0 +1,13 @@
+package com.alberto.tradepop.di
+
+import android.app.Application
+import org.kodein.di.DI
+import org.kodein.di.bind
+import org.kodein.di.singleton
+
+class AppDIModule(private val application: Application) : DIBaseModule("AppDIModule") {
+
+    override val builder: DI.Builder.() -> Unit = {
+        bind<Application>() with singleton { application }
+    }
+}
